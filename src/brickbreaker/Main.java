e2f3f1e04c5218e2553fc4e35aafd3f2eb2cd5fb
Prashant Nalli <nalliprashant@gmail.com>
2018-04-11 19:11:35 -0400

    Add files via upload

---
+++ src/brickbreaker/Main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package brickbreaker;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author danny
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    JFrame obj=new JFrame();
+    GamePlay gameplay=new GamePlay();
+    obj.setBounds(10, 10, 700, 600);
+    obj.setTitle("Break Out Ball");
+    obj.setResizable(false);
+    obj.setVisible(true);
+    obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    obj.add(gameplay);
+    
+    
+    
+    }
+    
+}
